3021d1823e85607f016f2ad1833ebe4d6cefd258
Emanuel Pinto Sampaio <emanuelpsinvest@gmail.com>
2026-07-01 01:51:45 -0300

    Delete T_EQ directory

--- T_EQ/BETA/L_v.py
+++
@@ -1,3 +0,0 @@
-def calcular_lv(T_gota):
-    L_vv = (25.00 - 0.02274 * T_gota) * 10**5
-    return L_vv
--- T_EQ/EXP_Y/sigma_s.py
+++
@@ -1,5 +0,0 @@
-
-def calculate_sigma_s(T_gota, m_s,m_w):
-    sigma_w = 7.610e-2 - 1.55e-4 * T_gota
-    sigma_ss = sigma_w + 2.77e-5 * m_s / m_w 
-    return sigma_ss
